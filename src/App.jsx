54b956338042145af30a96767fe11c0978928e19
Luana <luanaalves1298@gmail.com>
2026-05-20 19:08:23 -0300

    tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,73 @@
+import { Link } from "react-router-dom";
+<script src="https://kit.fontawesome.com/YOUR_KIT_CODE.js" crossorigin="anonymous"></script>
+import logoAquamarine from "/public/logo.png";
+
+
+import "./LoginPage.css";
+
+function LoginPage() {
+  return (
+    <div className="login-page">
+      <section className="login-container">
+        <div className="login-banner">
+          <div>
+            <h2>Bem-vindo de volta</h2>
+            <p>Faça login para continuar</p>
+          </div>
+        </div>
+
+        <div className="login-content">
+          <img
+            src={logoAquamarine}
+            alt="Logo Aquamarine"
+            className="login-logo"
+          />
+
+          <div className="login-title">
+            <h1>Bem-vindo de volta</h1>
+            <p>Acesso seguro a sistemas de controle de precisão</p>
+          </div>
+
+          <form className="login-form">
+            <div className="input-group">
+              <label>E-mail</label>
+              <input type="email" placeholder="Entre com seu e-mail" />
+            </div>
+
+            <div className="input-group">
+              <label>Senha</label>
+
+              <div className="password-input">
+                <input type="password" placeholder="Digite sua senha" />
+
+                <button type="button">
+                <i class="fa-solid fa-eye"></i>
+                </button>
+              </div>
+            </div>
+
+            <button type="submit" className="login-button">
+              Login
+            </button>
+          </form>
+
+          <div className="divider">
+            <span></span>
+            <p>ou</p>
+            <span></span>
+          </div>
+
+          <button className="google-button">
+            Entrar com Google
+          </button>
+
+          <p className="register-text">
+            Ainda não tem conta? <Link to="/cadastro">Cadastre-se</Link>
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default LoginPage
