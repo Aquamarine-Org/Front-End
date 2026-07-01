3d273698b99d2a57ad9d4741deb78b52dae8ef5f
guilhermecustodionieto@gmail.com <guilhermecustodionieto@gmail.com>
2026-07-01 00:21:15 -0300

    fix: problemas na cloudflare

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import { fileURLToPath, URL } from "node:url";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve("src"),
-      "@assets": resolve("src/assets"),
-      "@src": resolve("src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+      "@src": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
